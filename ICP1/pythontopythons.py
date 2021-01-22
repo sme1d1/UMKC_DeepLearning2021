357924e7e5a41e052aef0fe2830e17b745112ba3
sme1d1 <sme1d1@mail.umkc.edu>
2021-01-22 13:46:14 -0600

    ICP1 programs
    uploaded IPC1_1 & 2

---
+++ ICP1/pythontopythons.py
@@ -0,0 +1,8 @@
+# sme1d1 Scott McElfresh 1/22/2021
+
+# get user input for string
+x = input("Write a sentence with the word python in it and it will get changed to pythons \n")
+# replace lowercase python with Pythons
+y = x.replace("python", "pythons")
+# replace any uppercase Python with Pythons and print
+print(y.replace("Python", "Pythons"))
