6dea06f38d442dc0753c6302a4d859965f2b7422
sme1d1 <sme1d1@mail.umkc.edu>
2021-02-03 13:52:22 -0600

    improved coded comments and uploaded screenshots

--- ICP/ICP3/source/ICP3_1.py
+++ ICP/ICP3/source/ICP3_1.py
@@ -11,21 +11,23 @@ Scott McElfresh sme1d1 Deep Learning 2021 2/3/2021
 
 
 class Employee:
-    count = 0
+    count = 0  # set class variables to track number of employees and combined salaries
     salaryTotal = 0
 
+    # create and initialize constructors for name, family, salary, and department
     def __init__(self, n, f, s, d):
-        Employee.count += 1
-        Employee.salaryTotal = Employee.salaryTotal + s
+        Employee.count += 1  # update employee count
+        Employee.salaryTotal = Employee.salaryTotal + s # update combined salary total
         self.name = n
         self.family = f
         self.salary = s
         self.department = d
 
+    # create a method for averaging employee salaries
     def avg_salary(self):
         avgsal = float(Employee.salaryTotal) / Employee.count
         return avgsal
-
+    # create methods to print constructor values
     def print_name(self):
         print(self.name)
 
@@ -38,15 +40,17 @@ class Employee:
     def print_department(self):
         print(self.department)
 
-
+# create Fulltime Employee class that inherits properties of employee class
 class FulltimeEmployee(Employee):
     def __init__(self, n, f, s, d):
         Employee.__init__(self, n, f, s, d)
 
 
+# create instances of both classes
 a = Employee("Bill S.", "Preston, Esq.", 50000, "XLNT")
 b = FulltimeEmployee("Ted 'Theodore'", "Logan", 100000, "XLNT")
 
+# call member functions from instances of both classes
 a.print_name()
 a.print_family()
 a.print_salary()
@@ -57,4 +61,5 @@ b.print_family()
 b.print_salary()
 b.print_department()
 print("")
+# print average salary from combined employee salaries
 print("Average salary of all employees: {}".format(a.avg_salary()))
