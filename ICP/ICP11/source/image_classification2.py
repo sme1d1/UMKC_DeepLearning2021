9ba4216f205f834f5bfb162b08c1a5f3c4572084
sme1d1 <sme1d1@mail.umkc.edu>
2021-04-13 08:06:39 -0500

    code

---
+++ ICP/ICP11/source/image_classification2.py
@@ -0,0 +1,110 @@
+# Simple CNN model for CIFAR-10
+# Scott McElfresh sme1d1 4/12/2021
+
+import numpy
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.constraints import maxnorm
+from keras.optimizers import SGD
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.utils import np_utils
+from keras import backend as K
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+K.set_image_data_format('channels_last')
+
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+# load data
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+# normalize inputs from 0-255 to 0.0-1.0
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
+#print("Num Classes " + str(num_classes))
+# Create the model
+model = Sequential()
+model.add(
+    Conv2D(32, (3, 3), input_shape=X_train.shape[1:], padding='same', activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_last'))
+
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_last'))
+
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_last'))
+
+model.add(Flatten())
+model.add(Dropout(0.2))
+
+model.add(Dense(1024, activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+
+model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+
+model.add(Dense(num_classes, activation='softmax'))
+# Compile model
+epochs = 20
+lrate = 0.01
+decay = lrate / epochs
+sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+print(model.summary())
+# Fit the model
+history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=32, )
+model.save('cifar10_model.h5')
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1] * 100))
+
+print("Categories: 0: airplane, 1: automobile, 2: bird, 3: cat, 4: deer, 5: dog, 6: frog, 7: horse, 8: ship, 9: truck")
+
+result = np.argmax(model.predict(X_test), axis=-1)  # create array of predicted categories
+
+labelsAndTrainingImages = list(zip(X_test, y_test))
+# plot images and their labels
+for index, (image, label) in enumerate(labelsAndTrainingImages[:6]):
+    plt.subplot(2, 3, index + 1)
+    plt.imshow(image, interpolation='nearest')
+    plt.title("Label " + '%i' % result[index])
+
+# Plot accuracy
+plot2 = plt.figure(2)
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='lower right')
+
+# "Loss Plot"
+plot3 = plt.figure(3)
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
